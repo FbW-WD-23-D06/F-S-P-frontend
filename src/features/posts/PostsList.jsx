60c536b26525d688157a2781c8882acd505a103d
giacomotolari <tolarigiacomo@gmail.com>
2024-02-26 15:26:17 +0100

    refactor: convert PostsList to js file

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,9 +1,20 @@
 import { IonList } from "@ionic/react";
 import Post from "./Post";
 import posts from "../../data/posts.json";
+import { useEffect } from "react";
 
 export default function PostsList() {
   // fetch posts data and replace the posts from the JSON file
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("http://localhost:8888/posts");
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <IonList inset={true}>
       {posts.map((post) => (
@@ -12,4 +23,3 @@ export default function PostsList() {
     </IonList>
   );
 }
-
