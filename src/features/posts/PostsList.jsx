8fe7f27e2809e0fc101cb930a81b186ba76c910f
AxelH97 <hilde.axel@web.de>
2024-02-27 09:18:13 +0100

    update PostList with fetch, error and sort

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,25 +1,46 @@
-import { IonList } from "@ionic/react";
+import React, { useState, useEffect } from "react";
+import { IonList, IonSpinner, IonAlert } from "@ionic/react";
 import Post from "./Post";
-import posts from "../../data/posts.json";
-import { useEffect } from "react";
+import { formatDate } from "date-fns";
 
 export default function PostsList() {
-  // fetch posts data and replace the posts from the JSON file
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch("http://localhost:8888/posts");
-      const data = await response.json();
-      console.log(data);
+      try {
+        const response = await fetch("http://localhost:8888/posts");
+        if (!response.ok) {
+          throw new Error("Failed to fetch posts");
+        }
+        const data = await response.json();
+        // Sort posts by updatedAt, assuming updatedAt is a valid date string
+        data.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
+        setPosts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchPosts();
   }, []);
 
   return (
-    <IonList inset={true}>
-      {posts.map((post) => (
-        <Post key={post._id} {...post} />
-      ))}
-    </IonList>
+    <>
+      {loading && <IonSpinner />}
+      {error && <IonAlert isOpen={true} message={error} />}
+      <IonList inset={true}>
+        {posts.map((post) => (
+          <Post
+            key={post._id}
+            {...post}
+            updatedAt={formatDate(post.updatedAt, "dd MMMM yyyy, hh:mm a")}
+          />
+        ))}
+      </IonList>
+    </>
   );
 }
