e5e1c2d4fb1cae5714bf2ee85cb46b2d4b03fee5
panda <p4nd4@posteo.de>
2024-03-07 15:43:17 +0100

    clean up code

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,7 +6,7 @@ import { IonPage, IonContent } from "@ionic/react";
 import Header from "../features/navigation/layout/Header";
 import { endpoints } from "../data/api";
 
-interface LoginProps {}
+interface LoginProps {};
 
 export default function Login({}: LoginProps) {
   const { userState, dispatch } = useUserContext();
@@ -17,8 +17,6 @@ export default function Login({}: LoginProps) {
 
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log("login");
-
     try {
       const response = await fetch(endpoints.login, {
         method: "POST",
@@ -31,9 +29,7 @@ export default function Login({}: LoginProps) {
       if (response.ok) {
         const data = await response.json();
         console.log("yay!");
-        console.log(data);
         const loggedInUserName = data.user.userName;
-        // TODO: save data in context
         dispatch({ type: "login", userName: loggedInUserName, isLoggedIn: true });
         console.log(userState);
       }
