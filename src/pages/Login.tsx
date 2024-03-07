3fd36a583373f352156a2e0344c36ea204a1d526
panda <p4nd4@posteo.de>
2024-03-07 13:43:35 +0100

    Implement login form and backend integration

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,63 @@
 // pages/Login.tsx
+import { useState } from 'react';
 
 import { IonPage, IonContent } from "@ionic/react";
 import Header from "../features/navigation/layout/Header";
 
-interface LoginProps {}
+interface LoginProps {};
 
 export default function Login({}: LoginProps) {
-  return (
-    <IonPage>
-      <Header title="Login" />
-      <IonContent fullscreen>
-        <h1>Login</h1>
-        <input type="text" placeholder="name" />
-        <br />
-        <input type="password" placeholder="password"  />
-        <br />
-        <button>Login</button>
-    
-       {/* code for login here */}
-      </IonContent>
-    </IonPage>
+  const [ userName, setUsername ] = useState(null);
+  const [password, setPassword] = useState<string | null>(null);
+
+  async function onSubmit(event) {
+
+    event.preventDefault();
+    console.log('login');
+
+    try {
+      const response = await fetch(
+        'http://localhost:8888/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        //schema: userName
+        body: JSON.stringify({ userName, password })
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log('yay!');
+        console.log(data);
+    // TODO: save data in context
+      }
+    } catch (err) {
+      console.log(err)
+    };
+    }
+    return (
+      <IonPage>
+        <Header title="Login" />
+        <IonContent fullscreen>
+          <h1>Login</h1>
+          <form onSubmit={onSubmit}>
+          <input
+          type="text"
+          placeholder="name"
+          value={userName ?? ""}
+          id="userName"
+          onChange={(e) => setUsername(e.target.value)}/>
+          <br />
+          <input
+          type="password"
+          placeholder="password"
+          value={password ?? ""}
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}/>
+          <br/>
+          <button>Login</button>
+          </form>
+        </IonContent>
+      </IonPage>
   );
 }
