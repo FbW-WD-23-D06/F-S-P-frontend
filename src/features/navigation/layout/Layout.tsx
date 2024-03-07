91fd9caca438f7731253183c8a175b3c33da5da6
giacomotolari <tolarigiacomo@gmail.com>
2024-03-07 11:35:33 +0100

    refactor: select the tab only when the location.pathname is the same path of the tab.

--- src/features/navigation/layout/Layout.tsx
+++ src/features/navigation/layout/Layout.tsx
@@ -6,33 +6,45 @@ import {
   IonTabButton,
   IonTabs,
 } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
 import { chatbubbleOutline, person } from "ionicons/icons";
-import { Redirect } from "react-router";
+import { Redirect, useLocation } from "react-router";
 import Routes from "./Routes";
+import { paths } from "../routing/paths";
 
 interface LayoutProps {}
 
 export default function Layout({}: LayoutProps) {
+  const location = useLocation();
+
+  const isCurrentPathTab = (path: string) => location.pathname === path;
+
   return (
-    <IonReactRouter>
-      <Redirect from="*" to="/" />
-      {/* Redirect  to "/"" if there is not route with the curent path */}
+    <>
+      <Redirect from="*" to={paths.posts} />
+      {/* Redirect  to paths.posts if there is not route with the curent path */}
       <IonTabs>
         <IonRouterOutlet>
           <Routes />
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="Profile" href="/profile">
+          <IonTabButton
+            tab="Profile"
+            href={paths.profile}
+            selected={isCurrentPathTab(paths.profile)}
+          >
             <IonIcon aria-hidden="true" icon={person} />
             <IonLabel>Profile</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="Posts" href="/">
+          <IonTabButton
+            tab="Posts"
+            href={paths.posts}
+            selected={isCurrentPathTab(paths.posts)}
+          >
             <IonIcon aria-hidden="true" icon={chatbubbleOutline} />
             <IonLabel>Posts</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
-    </IonReactRouter>
+    </>
   );
 }
