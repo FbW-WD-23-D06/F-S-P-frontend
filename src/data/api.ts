0e7dc677c6a5290a6c567c6bc8e98911f7edb995
AxelH97 <hilde.axel@web.de>
2024-03-07 13:57:14 +0100

    login: refactoring

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,5 +1,8 @@
 const BASE_URL = "http://localhost:8888";
 
-const endpoints = { getAllPosts: `${BASE_URL}/posts` };
+const endpoints = {
+  getAllPosts: `${BASE_URL}/posts`,
+  login: `${BASE_URL}/users/login`,
+};
 
 export { BASE_URL, endpoints };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,63 +1,64 @@
 // pages/Login.tsx
-import { useState } from 'react';
+import { useState, FormEvent } from "react";
 
 import { IonPage, IonContent } from "@ionic/react";
 import Header from "../features/navigation/layout/Header";
+import { endpoints } from "../data/api";
 
-interface LoginProps {};
+interface LoginProps {}
 
 export default function Login({}: LoginProps) {
-  const [ userName, setUsername ] = useState(null);
+  const [userName, setUsername] = useState<string | null>(null);
   const [password, setPassword] = useState<string | null>(null);
 
-  async function onSubmit(event) {
-
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('login');
+    console.log("login");
 
     try {
-      const response = await fetch(
-        'http://localhost:8888/users/login', {
-        method: 'POST',
+      const response = await fetch(endpoints.login, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         //schema: userName
-        body: JSON.stringify({ userName, password })
+        body: JSON.stringify({ userName, password }),
       });
       if (response.ok) {
         const data = await response.json();
-        console.log('yay!');
+        console.log("yay!");
         console.log(data);
-    // TODO: save data in context
+        // TODO: save data in context
       }
     } catch (err) {
-      console.log(err)
-    };
+      console.log(err);
     }
-    return (
-      <IonPage>
-        <Header title="Login" />
-        <IonContent fullscreen>
-          <h1>Login</h1>
-          <form onSubmit={onSubmit}>
+  }
+  return (
+    <IonPage>
+      <Header title="Login" />
+      <IonContent fullscreen>
+        <h1>Login</h1>
+        <form onSubmit={onSubmit}>
           <input
-          type="text"
-          placeholder="name"
-          value={userName ?? ""}
-          id="userName"
-          onChange={(e) => setUsername(e.target.value)}/>
+            type="text"
+            placeholder="name"
+            value={userName ?? ""}
+            id="userName"
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <br />
           <input
-          type="password"
-          placeholder="password"
-          value={password ?? ""}
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}/>
-          <br/>
+            type="password"
+            placeholder="password"
+            value={password ?? ""}
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
           <button>Login</button>
-          </form>
-        </IonContent>
-      </IonPage>
+        </form>
+      </IonContent>
+    </IonPage>
   );
 }
