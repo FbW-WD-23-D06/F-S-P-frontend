baa0c2682471cbf7c94363ae11f2293345302c87
AxelH97 <hilde.axel@web.de>
2024-03-07 11:12:26 +0100

    create form and authentications

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,5 @@
+// pages/Login.tsx
+
 import { IonPage, IonContent } from "@ionic/react";
 import Header from "../features/navigation/layout/Header";
 
@@ -8,6 +10,13 @@ export default function Login({}: LoginProps) {
     <IonPage>
       <Header title="Login" />
       <IonContent fullscreen>
+        <h1>Login</h1>
+        <input type="text" placeholder="name" />
+        <br />
+        <input type="password" placeholder="password"  />
+        <br />
+        <button>Login</button>
+    
        {/* code for login here */}
       </IonContent>
     </IonPage>
