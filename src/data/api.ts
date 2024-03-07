851ae37e05dd574052438d49ee31dbf626f16621
friesenkatharina <friesen.katharina2022@gmail.com>
2024-03-07 14:22:25 +0100

    remove basis url, add endpoint

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,5 +1,8 @@
 const BASE_URL = "http://localhost:8888";
 
-const endpoints = { getAllPosts: `${BASE_URL}/posts` };
+const endpoints = {
+  getAllPosts: `${BASE_URL}/posts`,
+  register: `${BASE_URL}/users/register`,
+};
 
 export { BASE_URL, endpoints };
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -8,41 +8,41 @@ import {
   IonLabel,
 } from "@ionic/react";
 import Header from "../features/navigation/layout/Header";
+import { endpoints } from "../data/api";
 
 interface RegisterProps {}
 
 export default function Register({}: RegisterProps) {
   const [userName, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  
 
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
     // Hier würdest du deine Registrierungslogik implementieren,
     // z.B. einen API-Aufruf, um den Benutzer zu registrieren.
-    console.log({ userName, password}, typeof userName);
+    console.log({ userName, password }, typeof userName);
     try {
       const response = await postUser();
       console.log(response); // log the response from the backend
       // Handle the response as needed, e.g., show a success message or redirect the user
     } catch (error) {
-      console.error('Fehler bei der Registrierung:', error);
+      console.error("Fehler bei der Registrierung:", error);
       // Handle errors, e.g., show an error message to the user
     }
   };
 
   const postUser = async () => {
-    const url = "http://localhost:8888/users/register"
     const data = {
-      userName, password
-    }
-    const response = await fetch(url, {
+      userName,
+      password,
+    };
+    const response = await fetch(endpoints.register, {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
-    })
+      body: JSON.stringify(data),
+    });
     return response.json();
   };
 
