776d1353c32d1fd634439a5c6caa934b6795a27e
giacomotolari <tolarigiacomo@gmail.com>
2024-03-11 16:53:06 +0100

    refactor: register form validations from server

--- src/features/auhtentication/register/RegisterForm.tsx
+++ src/features/auhtentication/register/RegisterForm.tsx
@@ -3,6 +3,7 @@ import {
   IonInput,
   IonItem,
   IonLabel,
+  IonNote,
   IonSpinner,
 } from "@ionic/react";
 import { FormEvent, useState } from "react";
@@ -21,12 +22,18 @@ export default function RegisterForm({
   const [userName, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    userName: "",
+    password: "",
+  });
 
   const registerUser = async () => {
     const newUser = {
       userName,
       password,
     };
+
+    // block
     const response = await fetch(endpoints.register, {
       method: "POST",
       headers: {
@@ -36,11 +43,29 @@ export default function RegisterForm({
     });
     const data = await response.json();
     console.log("🚀 ~ registerUser ~ data:", data);
+    console.log("data.error:", data.error?.errors[0]);
     if (!response.ok) {
+      const inputTypeErrors = data.error?.errors;
+      if (inputTypeErrors) {
+        inputTypeErrors.forEach((inputTypeError: any) => {
+          if (inputTypeError["password"]) {
+            setError((prev) => ({
+              ...prev,
+              password: inputTypeError.password,
+            }));
+          }
+          if (inputTypeError["userName"]) {
+            setError((prev) => ({
+              ...prev,
+              userName: inputTypeError.userName,
+            }));
+          }
+        });
+      }
       throw new Error(
-        data.errors?.error?.message ||
-          data.message ||
-          "Registration failed due to an error."
+        JSON.stringify(data.error.errors) ||
+          data.error.message ||
+          "Registration failed."
       );
     }
     return data;
@@ -50,8 +75,7 @@ export default function RegisterForm({
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await registerUser();
-      console.log("response:", response);
+      await registerUser();
       setToastInfo({
         message: "Registration successful!",
         color: "success",
@@ -75,19 +99,25 @@ export default function RegisterForm({
       <IonItem>
         <IonLabel position="floating">User Name</IonLabel>
         <IonInput
+          className={``}
           value={userName}
           onIonInput={(e) => setUsername(e.detail.value!)}
           clearInput
+          minlength={2}
         ></IonInput>
+        {error.userName && <IonNote color="danger">{error.userName}</IonNote>}
       </IonItem>
       <IonItem>
         <IonLabel position="floating">Password</IonLabel>
         <IonInput
+          className={``}
           type="password"
           value={password}
           onIonInput={(e) => setPassword(e.detail.value!)}
           clearInput
+          minlength={8}
         ></IonInput>
+        {error.password && <IonNote color="danger">{error.password}</IonNote>}
       </IonItem>
       <IonButton
         disabled={isToastVisible || loading}
