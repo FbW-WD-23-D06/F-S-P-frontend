27756551137e18160a261fe8faa2ce52daf487e5
giacomotolari <tolarigiacomo@gmail.com>
2024-03-12 09:54:35 +0100

    refator: validations return with just  atets and not object

--- src/features/auhtentication/validations.ts
+++ src/features/auhtentication/validations.ts
@@ -1,28 +1,27 @@
-const validations = {
-  userName: (userName: string) => {
+interface Validations {
+  userName: (userName: string) => string;
+  password: (password: string) => string;
+}
+
+const validations: Validations = {
+  userName: (userName) => {
     if (
       typeof userName !== "string" ||
       userName.length < 2 ||
       userName.length > 15
     ) {
-      return {
-        errorMessage:
-          "The user name is required and must contain at least 2 and max. 15 characters.",
-      };
+      return "The user name is required and must contain at least 2 and max. 15 characters.";
     }
     return "valid";
   },
-  password: (password: string) => {
+  password: (password) => {
     const passwordRegex =
       /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[?!])[0-9a-zA-Z?!]{8,}$/;
     if (!passwordRegex.test(password)) {
-      return {
-        errorMessage:
-          "The password must contain at least 1 number, 1 lowercase and uppercase character, one special character and be at least 8 characters long.",
-      };
+      return "The password must contain at least 1 number, 1 lowercase and uppercase character, one special character and be at least 8 characters long.";
     }
     return "valid";
   },
 };
 
-export default validations;
+export { validations };
