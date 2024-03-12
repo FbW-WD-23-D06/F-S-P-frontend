c0f2b3284e971da8e6ba4ff038e5340e3de32590
giacomotolari <tolarigiacomo@gmail.com>
2024-03-12 10:27:49 +0100

    feature: login endoint

--- src/data/api.ts
+++ src/data/api.ts
@@ -3,6 +3,7 @@ const BASE_URL = "http://localhost:8888";
 const endpoints = {
   getAllPosts: `${BASE_URL}/posts`,
   register: `${BASE_URL}/users/register`,
+  login: `${BASE_URL}/users/login`,
 };
 
 export { BASE_URL, endpoints };
