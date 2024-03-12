7c0558932d990cfc786de36cade0c0af494c02a0
giacomotolari <tolarigiacomo@gmail.com>
2024-03-12 11:15:52 +0100

    feature: userReducer

---
+++ src/features/auhtentication/userReducer.ts
@@ -0,0 +1,29 @@
+interface UserState {
+  userName: string | null;
+  password: string | null;
+  isLoggedIn: boolean;
+}
+
+interface UserAction {
+  type: "login";
+  field: keyof UserState;
+  value: string;
+}
+
+export default function UserReducer(state: UserState, action: UserAction) {
+  console.log(action);
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        // set username:
+        [action.field]: action.value,
+        isLoggedIn: true,
+      };
+    default:
+      break;
+  }
+  return state;
+}
+
+export type { UserState, UserAction };
