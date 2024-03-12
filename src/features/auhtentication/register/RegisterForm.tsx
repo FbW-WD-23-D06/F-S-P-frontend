b9c0cb98a066347f359a74cc5368546cc2d8d6a3
giacomotolari <tolarigiacomo@gmail.com>
2024-03-12 10:20:18 +0100

    refactor: disable register button if validations are false and display in the toast the right message in the right time

--- src/features/auhtentication/register/RegisterForm.tsx
+++ src/features/auhtentication/register/RegisterForm.tsx
@@ -6,7 +6,7 @@ import {
   IonNote,
   IonSpinner,
 } from "@ionic/react";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
 import { endpoints } from "../../../data/api";
 import { ToastInfo } from "../../../hooks/useToastManager";
 import { validations } from "../validations";
@@ -23,39 +23,13 @@ export default function RegisterForm({
   const [userName, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState({
+  const [errorMessage, setErrorMessage] = useState({
     userName: "",
     password: "",
   });
 
-  useEffect(() => {
-    const userNameValidation = validations.userName(userName);
-    const passwordValidation = validations.password(password);
-
-    if (validations.userName(userName) === "valid") {
-      setError((prev) => ({
-        ...prev,
-        userName: "",
-      }));
-    } else {
-      setError((prev) => ({
-        ...prev,
-        userName: validations.userName(userName),
-      }));
-    }
-    if (validations.password(password) === "valid") {
-      setError((prev) => ({
-        ...prev,
-        password: "",
-      }));
-    }
-    if (validations.password(password) !== "valid") {
-      setError((prev) => ({
-        ...prev,
-        password: validations.password(password),
-      }));
-    }
-  }, [userName, password]);
+  const isUserNameValid = validations.userName(userName) === "valid";
+  const isPasswordValid = validations.password(password) === "valid";
 
   const registerUser = async () => {
     const newUser = {
@@ -63,27 +37,27 @@ export default function RegisterForm({
       password,
     };
 
-    const response = await fetch(endpoints.register, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newUser),
-    });
+    try {
+      const response = await fetch(endpoints.register, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      });
 
-    const data = await response.json();
-    console.log("🚀 ~ registerUser ~ data:", data);
-    console.log("data.error:", data.error?.errors[0]);
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(
+          data.error?.message || data?.message || "Registration failed."
+        );
+      }
 
-    if (!response.ok) {
-      const inputTypeErrors = data.error?.errors;
-      throw new Error(
-        JSON.stringify(inputTypeErrors) ||
-          data.error.message ||
-          "Registration failed."
-      );
+      return data;
+    } catch (error) {
+      console.error("🚀 ~ registerUser ~ error:", error);
+      throw error;
     }
-    return data;
   };
 
   const handleRegister = async (e: FormEvent) => {
@@ -99,7 +73,7 @@ export default function RegisterForm({
     } catch (err: any) {
       console.error("Register user error:", err);
       setToastInfo({
-        message: err,
+        message: err.message,
         color: "danger",
         duration: 3000,
         buttons: "dismiss",
@@ -112,7 +86,7 @@ export default function RegisterForm({
   const handleUserNameChange = (e: CustomEvent) => {
     setUsername(e.detail.value);
     const userNameValidation = validations.userName(userName);
-    setError((prev) => ({
+    setErrorMessage((prev) => ({
       ...prev,
       userName: userNameValidation,
     }));
@@ -121,7 +95,7 @@ export default function RegisterForm({
   const handlePasswordChange = (e: CustomEvent) => {
     setPassword(e.detail.value);
     const passwordValidation = validations.password(password);
-    setError((prev) => ({
+    setErrorMessage((prev) => ({
       ...prev,
       password: passwordValidation,
     }));
@@ -133,34 +107,34 @@ export default function RegisterForm({
         <IonLabel position="floating">User Name</IonLabel>
         <IonInput
           aria-label="User Name"
-          className={``}
           value={userName}
           onIonInput={handleUserNameChange}
           clearInput
           minlength={2}
           counter
-        ></IonInput>
-        {error.userName && error.userName !== "valid" && (
-          <IonNote color="danger">{error.userName}</IonNote>
+        />
+        {errorMessage.userName && !isUserNameValid && (
+          <IonNote color="danger">{errorMessage.userName}</IonNote>
         )}
       </IonItem>
       <IonItem>
         <IonLabel position="floating">Password</IonLabel>
         <IonInput
           aria-label="Password"
-          className={``}
           type="password"
           value={password}
           onIonInput={handlePasswordChange}
           clearInput
           minlength={8}
-        ></IonInput>
-        {error.password && error.password !== "valid" && (
-          <IonNote color="danger">{error.password}</IonNote>
+        />
+        {errorMessage.password && !isPasswordValid && (
+          <IonNote color="danger">{errorMessage.password}</IonNote>
         )}
       </IonItem>
       <IonButton
-        disabled={isToastVisible || loading}
+        disabled={
+          isToastVisible || loading || !isUserNameValid || !isPasswordValid
+        }
         type="submit"
         expand="block"
         style={{ marginTop: 20 }}
