22fd37ffc48d3c818b7bfbd782e3dbca3c995296
giacomotolari <tolarigiacomo@gmail.com>
2024-03-14 11:30:08 +0100

    data: getAuthUserData endpoint

--- src/data/api.ts
+++ src/data/api.ts
@@ -4,6 +4,7 @@ const endpoints = {
   getAllPosts: `${BASE_URL}/posts`,
   register: `${BASE_URL}/users/register`,
   login: `${BASE_URL}/users/login`,
+  getAuthUserData: `${BASE_URL}/users/auth-user-data`,
 };
 
 export { BASE_URL, endpoints };
