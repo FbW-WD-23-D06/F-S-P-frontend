60a37df3b91573f438108a10bc42d5eee70dcd7c
giacomotolari <tolarigiacomo@gmail.com>
2024-03-14 11:29:43 +0100

    feature: fetchUserData and set the userState

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import { IonApp, setupIonicReact } from "@ionic/react";
+import { useEffect } from "react";
 import Layout from "./features/navigation/layout/Layout";
 // import all styles and theme files from ionic and custom files
 import "./styles/styles_imports";
 import { IonReactRouter } from "@ionic/react-router";
+import axios from "axios";
+import { endpoints } from "./data/api";
+import { useAppContext } from "./contexts/AppContext";
 
 setupIonicReact();
 
 export default function App() {
+  const { dispatchUser } = useAppContext();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(endpoints.getAuthUserData, {
+          withCredentials: true,
+        });
+        const userData = response.data.user;
+        console.log("🚀 ~ fetchUserData ~ userData:", userData);
+        dispatchUser({ type: "fetch-user-data", value: userData });
+      } catch (err) {
+        console.log("err", err);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
   return (
     <IonApp>
       <IonReactRouter>
-      <Layout />
+        <Layout />
       </IonReactRouter>
     </IonApp>
   );
