8d43914aa606b68289e5bfc63315569308b38b86
giacomotolari <tolarigiacomo@gmail.com>
2024-03-18 13:53:03 +0100

    refactor: use handleLogout function in the onclick

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -8,16 +8,26 @@ import {
   IonText,
 } from "@ionic/react";
 import Header from "../features/navigation/layout/Header";
-import { useContext } from 'react';
 import { logInOutline, personAdd } from "ionicons/icons";
 import { paths } from "../features/navigation/routing/paths";
 import { useAppContext } from "../contexts/AppContext";
 import { endpoints } from "../data/api";
 
-
 export default function Profile() {
   const { userState, dispatchUser } = useAppContext();
   console.log(userState.isLoggedIn);
+
+  const handleLogout = async () => {
+    try {
+      await fetch(endpoints.logout, {
+        method: "POST",
+        credentials: "include",
+      });
+      dispatchUser({ type: "logout" });
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
 
   return (
     <IonPage>
@@ -31,29 +41,17 @@ export default function Profile() {
             <IonIcon icon={personAdd} slot="start" />
             <IonLabel>Register</IonLabel>
           </IonItem>
-          { userState.isLoggedIn ?
-            <IonItem button onClick={
-              async () => {
-                const response = await fetch(endpoints.logout, {
-                  method: "POST", headers: {
-                    "Content-Type": "application/json",
-                  }
-                });
-                console.log(response);
-                if (response.ok) {
-                  console.log('logout')
-                  dispatchUser({type: "logout"});
-                }
-              }}>
+          {userState.isLoggedIn ? (
+            <IonItem button onClick={handleLogout}>
               <IonIcon icon={logInOutline} slot="start" />
               <IonLabel>Logout</IonLabel>
             </IonItem>
-            :
+          ) : (
             <IonItem button routerLink={paths.login}>
               <IonIcon icon={logInOutline} slot="start" />
               <IonLabel>Login</IonLabel>
             </IonItem>
-          }
+          )}
         </IonList>
       </IonContent>
     </IonPage>
