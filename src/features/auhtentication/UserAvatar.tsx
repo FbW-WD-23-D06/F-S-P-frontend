8b6114ec323ba57f1c96a3ed340771ff35e93425
giacomotolari <tolarigiacomo@gmail.com>
2024-03-20 09:52:05 +0100

    styles: avatar margin and sizes. refactor: add avatar user in the header component

--- src/features/auhtentication/UserAvatar.tsx
+++ src/features/auhtentication/UserAvatar.tsx
@@ -8,7 +8,12 @@ interface UserAvatarProps {}
 export default function UserAvatar({}: UserAvatarProps) {
   const history = useHistory();
   return (
-    <IonAvatar onClick={() => history.push(paths.profile)}>
+    <IonAvatar
+      onClick={() => history.push(paths.profile)}
+      slot="end"
+      className="ion-margin"
+      style={{ height: "44px", width: "44px" }}
+    >
       <img
         alt="Silhouette of a person's head"
         src="https://ionicframework.com/docs/img/demos/avatar.svg"
--- src/features/navigation/layout/Header.tsx
+++ src/features/navigation/layout/Header.tsx
@@ -1,14 +1,18 @@
 import { IonHeader, IonToolbar, IonTitle } from "@ionic/react";
+import UserAvatar from "../../auhtentication/UserAvatar";
+import { useAppContext } from "../../../contexts/AppContext";
 
 interface HeaderProps {
   title: string;
 }
 
 export default function Header({ title }: HeaderProps) {
+  const { userState } = useAppContext();
   return (
     <IonHeader>
       <IonToolbar>
         <IonTitle>{title}</IonTitle>
+        {userState.isLoggedIn && <UserAvatar />}
       </IonToolbar>
     </IonHeader>
   );
