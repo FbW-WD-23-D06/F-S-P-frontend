62b88d8d4293068482d9b97d3f3ddf823bc9c68e
giacomotolari <tolarigiacomo@gmail.com>
2024-03-20 09:44:20 +0100

    feature: UserAvatar component

---
+++ src/features/auhtentication/UserAvatar.tsx
@@ -0,0 +1,18 @@
+import { IonAvatar } from "@ionic/react";
+import {} from "react";
+import { useHistory } from "react-router";
+import { paths } from "../navigation/routing/paths";
+
+interface UserAvatarProps {}
+
+export default function UserAvatar({}: UserAvatarProps) {
+  const history = useHistory();
+  return (
+    <IonAvatar onClick={() => history.push(paths.profile)}>
+      <img
+        alt="Silhouette of a person's head"
+        src="https://ionicframework.com/docs/img/demos/avatar.svg"
+      />
+    </IonAvatar>
+  );
+}
